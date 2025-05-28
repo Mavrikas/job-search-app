64f0243e4ebead1d1b3f03412e798f28249d484a
amav <athmavrikas@gmail.com>
2025-05-28 10:17:16 +0300

    updated loading.tsx

--- src/app/[lang]/loading.tsx
+++ src/app/[lang]/loading.tsx
@@ -1,10 +1,16 @@
+import { ProgressSpinner } from 'primereact/progressspinner';
+
 export default function Loading() {
   return (
     <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
-        <div className="border-solid border-3 rounded-lg p-5 bg-[#fafafa] max-w-[800px] min-w-[400px] m-1">
-          <h1 className="text-2xl font-bold">Loading...</h1>
-          <p className="text-lg">Please wait while we fetch the data.</p>
+        <div className="border-solid border-3 rounded-lg p-5 bg-[#fafafa] max-w-[800px] min-w-[400px] m-1 flex flex-col items-center justify-center gap-4">
+          <ProgressSpinner
+            style={{ width: '50px', height: '50px' }}
+            strokeWidth="8"
+            fill="var(--surface-ground)"
+            animationDuration=".5s"
+          />
         </div>
       </main>
     </div>
